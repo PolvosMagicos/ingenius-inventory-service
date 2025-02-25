9f067aaa687854cab6d71f278da22a048e9c27a3
Yomelx <140577890+Yomelx@users.noreply.github.com>
2025-02-25 14:25:31 -0500

    fix: fixing routes

---
+++ src/delivery/routes/mod.rs
@@ -0,0 +1,20 @@
+use crate::{delivery::controllers::handlers, student};
+use actix_web::web;
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("")
+            .route(web::post().to(handlers::create_delivery))
+            .route(web::get().to(handlers::get_all_deliveries)),
+    )
+    .service(
+        web::resource("/{id}")
+            .route(web::get().to(handlers::get_delivery))
+            .route(web::patch().to(handlers::update_delivery))
+            .route(web::delete().to(handlers::delete_delivery)),
+    )
+    .service(
+        web::resource("/{id}/students")
+            .route(web::get().to(student::controllers::handlers::get_students_by_delivery)),
+    );
+}
